9ad44cff25dca9da46027103f75dc63cbdbe136f
adeyopaul <adeyopaul53@gmail.com>
2025-11-22 22:52:50 +0100

    feat: Sign up pageview

--- lib/presentation/views/screens/signUpPersonalDetails.dart
+++ lib/presentation/views/screens/signUpPersonalDetails.dart
@@ -11,92 +11,94 @@ class Signuppersonaldetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: EdgeInsets.only(top: AppSpacing.md),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Sign up',
-                      style: Theme.of(context).textTheme.displayMedium,
-                    ),
-                    Text(
-                      'Personal Details',
-                      style: Theme.of(context).textTheme.headlineLarge,
-                    ),
-                  ],
-                ),
-                CircularStepIndicator(currentStep: 1, totalSteps: 4),
-              ],
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: AppSpacing.md),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Sign up',
+                        style: Theme.of(context).textTheme.displayMedium,
+                      ),
+                      Text(
+                        'Personal Details',
+                        style: Theme.of(context).textTheme.headlineLarge,
+                      ),
+                    ],
+                  ),
+                  CircularStepIndicator(currentStep: 1, totalSteps: 4),
+                ],
+              ),
             ),
-          ),
-          SizedBox(height: 6.h),
-          Padding(
-            padding: EdgeInsets.only(top: AppSpacing.md),
-            child: Text(
-              'First Name',
-              style: Theme.of(context).textTheme.headlineMedium,
+            SizedBox(height: 6.h),
+            Padding(
+              padding: EdgeInsets.only(top: AppSpacing.md),
+              child: Text(
+                'First Name',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(
-              top: AppSpacing.sm,
-              // bottom: AppSpacing.ms,
+            Padding(
+              padding: EdgeInsets.only(
+                top: AppSpacing.sm,
+                // bottom: AppSpacing.ms,
+              ),
+              child: Textformfield(
+                title: 'Enter your First name',
+                isPassword: false,
+              ),
             ),
-            child: Textformfield(
-              title: 'Enter your First name',
-              isPassword: false,
+            Padding(
+              padding: EdgeInsets.only(top: AppSpacing.ms),
+              child: Text(
+                'Last Name',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: AppSpacing.ms),
-            child: Text(
-              'Last Name',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Padding(
+              padding: EdgeInsets.only(top: AppSpacing.sm, bottom: AppSpacing.ms),
+              child: Textformfield(
+                title: 'Enter your Last name',
+                isPassword: false,
+              ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: AppSpacing.sm, bottom: AppSpacing.ms),
-            child: Textformfield(
-              title: 'Enter your Last name',
-              isPassword: false,
+            Padding(
+              padding: EdgeInsets.only(top: AppSpacing.xs),
+              child: Text(
+                'Date of Birth',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: AppSpacing.xs),
-            child: Text(
-              'Date of Birth',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Padding(
+              padding: EdgeInsets.only(top: AppSpacing.sm, bottom: AppSpacing.ms),
+              child: Textformfield(
+                title: 'Enter your Date of Birth',
+                isPassword: false,
+              ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: AppSpacing.sm, bottom: AppSpacing.ms),
-            child: Textformfield(
-              title: 'Enter your Date of Birth',
-              isPassword: false,
+            Padding(
+              padding: EdgeInsets.only(top: AppSpacing.xs),
+              child: Text(
+                'BVN',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: AppSpacing.xs),
-            child: Text(
-              'BVN',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Padding(
+              padding: EdgeInsets.only(top: AppSpacing.sm, bottom: AppSpacing.ms),
+              child: Textformfield(
+                title: 'Enter your BVN',
+                isPassword: false,
+              ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: AppSpacing.sm, bottom: AppSpacing.ms),
-            child: Textformfield(
-              title: 'Enter your BVN',
-              isPassword: false,
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
